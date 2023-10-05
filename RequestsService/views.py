2c19cc85188c7286f8f5c5e0a967ba680a2afe0d
azare242 <azare242@gmail.com>
2023-10-06 01:04:51 +0330

    bobobo

--- RequestsService/views.py
+++ RequestsService/views.py
@@ -12,11 +12,24 @@ def submit_request(request):
     
     try:
         client_ip = request.META.get('REMOTE_ADDR')
+        # print(Customer.objects.get(national_id = request.data['national_id']))
+        customer_requests = Customer.objects.filter(national_id = request.data['national_id'])
+        if len(customer_requests) != 0:
+            for req in customer_requests:
+                if req.state != "R":
+                    return Response({"message": "Duplicate National_ID"})
+            
+            customer_requests[0].state = "P"
+            customer_requests[0].save()
+            return Response({"message": "Your Request Submited Again"})
         new_customer = Customer(email=request.data['email'], last_name=request.data['last_name'], national_id=request.data['national_id'], user_ip=client_ip, state="P")
         new_customer.save()
-        return Response({"message": "DONE"}, status=200)
+        return Response({"message": "Your Request Submited"}, status=200)
     except:
         return Response({"message": "ERROR"}, status=400)
+    
+    
+
     
     
 @api_view(['POST'])
