9f6ef70caafa8e4c6b28835e02f090b29795e99b
azare242 <azare242@gmail.com>
2023-10-06 00:49:21 +0330

    start cc hw1

--- ahora_API/settings.py
+++ ahora_API/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'boys',
+    # 'boys',
     'RequestsService'
     
 ]
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.json.JSONMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'ahora_API.urls'
@@ -128,3 +130,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# AUTH_USER_MODEL = 'RequestsService.CustomUser'
+
+HASHID_FIELD_SALT = '$oknhgfcv$!#$!@$mkloiuy8767$#!$@#8490398!#@$!'  # Replace with your secret salt
+HASHID_FIELD_ALLOW_INT_LOOKUP = True  # Allow looking up by integer value
