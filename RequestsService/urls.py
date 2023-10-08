4f4cbef0e3d3b1f2deeec5025c38d38000a41fe3
azare242 <azare242@gmail.com>
2023-10-08 23:41:33 +0330

    typo

--- RequestsService/urls.py
+++ RequestsService/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('ping/', ping, name='ping'),
     path("submit/", submit_request, name='submit_request'),
     path("status/", get_status, name="status"),
-    path('runs3/', run_s3, name='s3'),
+    # path('runs3/', run_s3, name='s3'),
     # path("tsimg/", test_images, name='testi')
 ]
