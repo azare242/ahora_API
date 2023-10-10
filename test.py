d92b7153992f430dfb287bad88d26422eff6d777
azare242 <azare242@gmail.com>
2023-10-10 10:38:13 +0000

    ignore

--- test.py
+++
@@ -1,4 +0,0 @@
-import json
-x = '{"result": {'aces': [{'attributes': [], 'confidence': 99.9994735717773, 'coordinates': {'height': 487, 'width': 487, 'xmax': 1010, 'xmin': 523, 'ymax': 612, 'ymin': 125}, 'face_id': '5ddd8f355dfd1c134272c432348e20775866314d7e7db972f30a3911efadc641', 'landmarks': []}]}, 'status': {'text': '', 'type': 'success'}}'
-
-print(json.loads(x))
